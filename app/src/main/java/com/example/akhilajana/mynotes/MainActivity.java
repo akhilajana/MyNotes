d291e2f1a4e23d46f3d263dab48a038c95a2f347
Akhila Jana <akhilajana@wifi_stu-10-38-8-137.uncc.edu>
2018-04-06 19:12:21 -0400

    enhanced and working

--- app/src/main/java/com/example/akhilajana/mynotes/MainActivity.java
+++ app/src/main/java/com/example/akhilajana/mynotes/MainActivity.java
@@ -2,101 +2,255 @@ package com.example.akhilajana.mynotes;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Image;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
-import java.util.List;
+import java.lang.reflect.Method;
+import java.util.Random;
+import java.util.UUID;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private static final String TAG = "PdfCreatorActivity";
-    private EditText mContentEditText;
-    private Button mCreateButton;
+    private EditText mInputText, mDocName, mInputTitle;
+    private Button mCreatePdfButton;
     private File pdfFile;
-    final private int REQUEST_CODE_ASK_PERMISSIONS = 111;
+    private ImageButton mAttachImg, btnBold,btnItalic, btnUnderline;
+    private Document document;
+
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 100;
+    private static final String TAG = "PdfCreatorActivity";
+
+    private String flag="null";
+
+    private int selectionStart,selectionEnd;
+    private String startingText,endingText, selectedText;
+
+
+//    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mContentEditText = (EditText) findViewById(R.id.edit_text_content);
-        mCreateButton = (Button) findViewById(R.id.button_create);
-        mCreateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mContentEditText.getText().toString().isEmpty()){
-                    mContentEditText.setError("Body is empty");
-                    mContentEditText.requestFocus();
-                    return;
-                }
-                try {
-                    createPdfWrapper();
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (DocumentException e) {
-                    e.printStackTrace();
-                }
+        document = new Document();
+ //       mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        mDocName = (EditText) findViewById(R.id.edit_file_name);
+        mInputTitle = (EditText) findViewById(R.id.edit_title);
+        mInputText = (EditText) findViewById(R.id.edit_text_content);
+        mCreatePdfButton = (Button) findViewById(R.id.button_create);
+       // mAttachImg = (ImageView) findViewById(R.id.attach_Img);
+        btnBold = findViewById(R.id.bold_Img);
+        btnItalic = findViewById(R.id.italic_Img);
+        btnUnderline = findViewById(R.id.underline_Img);
+
+        if(Build.VERSION.SDK_INT>=24){
+            try{
+                Method m = StrictMode.class.getMethod("disableDeathOnFileUriExposure");
+                m.invoke(null);
+            }catch(Exception e){
+                e.printStackTrace();
             }
-        });
+        }
+
+        mCreatePdfButton.setOnClickListener(this);
+//        mAttachImg.setOnClickListener(this);
+
+        btnBold.setOnClickListener(this);
+        btnItalic.setOnClickListener(this);
+        btnUnderline.setOnClickListener(this);
+
     }
 
-    private void createPdfWrapper() throws FileNotFoundException,DocumentException{
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId())
+        {
+//            case R.id.attach_Img:
+//
+//                chooseImage();
+//
+//                break;
 
-        int hasWriteStoragePermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if (hasWriteStoragePermission != PackageManager.PERMISSION_GRANTED) {
+            case R.id.button_create:
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (!shouldShowRequestPermissionRationale(Manifest.permission.WRITE_CONTACTS)) {
-                    showMessageOKCancel("You need to allow access to Storage",
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                                        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                                                REQUEST_CODE_ASK_PERMISSIONS);
-                                    }
-                                }
-                            });
-                    return;
+                try {
+                    validateInput();
+                    createPdfWrapper();
                 }
+                catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                catch (DocumentException e) {
+                        e.printStackTrace();
+                    }
+                break;
 
-                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                        REQUEST_CODE_ASK_PERMISSIONS);
-            }
+            case R.id.bold_Img:
+
+                if(!document.isOpen())
+                document.open();
+
+                selectionStart = mInputText.getSelectionStart();
+                selectionEnd = mInputText.getSelectionEnd();
+
+                startingText = mInputText.getText().toString().substring(0, selectionStart);
+                selectedText = mInputText.getText().toString().substring(selectionStart, selectionEnd);
+                endingText = mInputText.getText().toString().substring(selectionEnd);
+
+                mInputText.setText(Html.fromHtml(startingText + "<b>" + selectedText + "</b>" + endingText));
+                flag="bold";
+
+                break;
+
+            case R.id.italic_Img:
+
+                if(!document.isOpen())
+                    document.open();
+
+                selectionStart = mInputText.getSelectionStart();
+                selectionEnd = mInputText.getSelectionEnd();
+
+                startingText = mInputText.getText().toString().substring(0, selectionStart);
+                selectedText = mInputText.getText().toString().substring(selectionStart, selectionEnd);
+                endingText = mInputText.getText().toString().substring(selectionEnd);
+
+                mInputText.setText(Html.fromHtml(startingText + "<i>" + selectedText + "</i>" + endingText));
+                flag="italic";
+
+                break;
+
+            case R.id.underline_Img:
+
+                if(!document.isOpen())
+                    document.open();
+
+                selectionStart = mInputText.getSelectionStart();
+                selectionEnd = mInputText.getSelectionEnd();
+
+                startingText = mInputText.getText().toString().substring(0, selectionStart);
+                selectedText = mInputText.getText().toString().substring(selectionStart, selectionEnd);
+                endingText = mInputText.getText().toString().substring(selectionEnd);
+
+                mInputText.setText(Html.fromHtml(startingText + "<u>" + selectedText + "</u>" + endingText));
+                flag="underline";
+
+                break;
+
+        }
+
+    }
+
+    private void validateInput()
+    {
+        if (mInputText.getText().toString().isEmpty()){
+            mInputText.setError("Body is empty");
+            mInputText.requestFocus();
             return;
-        }else {
-            createPdf();
+        }
+        else if (mInputTitle.getText().toString().isEmpty()){
+            mInputTitle.setError("Title is empty");
+            mInputTitle.requestFocus();
+            return;
+        }
+        else if(mDocName.getText().toString().isEmpty())
+        {
+            mDocName.setError("Document name is empty");
+            mDocName.requestFocus();
+            return;
         }
     }
 
-    private void showMessageOKCancel(String s, DialogInterface.OnClickListener onClickListener) {
+    private void createPdfWrapper() throws FileNotFoundException,DocumentException
+    {
+
+        int hasWriteStoragePermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (hasWriteStoragePermission != PackageManager.PERMISSION_GRANTED)
+        {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            {
+                if (!shouldShowRequestPermissionRationale(Manifest.permission.WRITE_CONTACTS))
+                {
+                    showMessageOKCancel("You need to allow access to Storage", new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which)
+                        {
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                            {
+                                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_ASK_PERMISSIONS);
+                            }
+                        }
+                    });
+                    return;
+                }
+
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_ASK_PERMISSIONS);
+            }
+            return;
+        }
+        else
+        {
+            try {
+                createPdf();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void showMessageOKCancel(String msg, DialogInterface.OnClickListener onClickListener) {
 
         new AlertDialog.Builder(this)
-                .setMessage("Message")
+                .setMessage(msg)
                 .setPositiveButton("OK", onClickListener)
                 .setNegativeButton("Cancel", null)
                 .create()
@@ -105,9 +259,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
+        switch (requestCode)
+        {
             case REQUEST_CODE_ASK_PERMISSIONS:
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
                     // Permission Granted
                     try {
                         createPdfWrapper();
@@ -116,18 +272,21 @@ public class MainActivity extends AppCompatActivity {
                     } catch (DocumentException e) {
                         e.printStackTrace();
                     }
-                } else {
+                }
+                else
+                {
                     // Permission Denied
-                    Toast.makeText(this, "WRITE_EXTERNAL Permission Denied", Toast.LENGTH_SHORT)
-                            .show();
+                    Toast.makeText(this, "WRITE_EXTERNAL Permission Denied", Toast.LENGTH_SHORT).show();
                 }
                 break;
+
             default:
                 super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 
-    private void createPdf() throws FileNotFoundException, DocumentException
+
+    private void createPdf() throws IOException, DocumentException
     {
         File docsFolder = new File(Environment.getExternalStorageDirectory() + "/Documents");
         if (!docsFolder.exists()) {
@@ -135,18 +294,62 @@ public class MainActivity extends AppCompatActivity {
             Log.i(TAG, "Created a new directory for PDF");
         }
 
-        pdfFile = new File(docsFolder.getAbsolutePath(),"HelloWorld.pdf");
+        pdfFile = new File(docsFolder.getAbsolutePath(), mDocName.getText().toString());
         OutputStream output = new FileOutputStream(pdfFile);
-        Document document = new Document();
         PdfWriter.getInstance(document, output);
+
+        Font f=new Font(Font.FontFamily.TIMES_ROMAN,30.0f,Font.BOLD, BaseColor.BLACK);
+        Paragraph titleParagraph=new Paragraph(mInputTitle.getText().toString(),f);
+
+        titleParagraph.setAlignment(Paragraph.ALIGN_CENTER);
+
         document.open();
-        document.add(new Paragraph(mContentEditText.getText().toString()));
+
+        document.add(titleParagraph);
+        document.setMargins(1,1,1,1);
+
+        if(flag.equals("bold"))
+        {
+            //Format bold
+            Font f1=new Font(Font.FontFamily.TIMES_ROMAN,10.0f,Font.BOLD, BaseColor.BLACK);
+            Paragraph b1=new Paragraph(selectedText,f1);
+            document.add(new Paragraph(startingText));
+            document.add(b1);
+            document.add(new Paragraph(endingText));
+        }
+        else if(flag.equals("italic"))
+        {
+            //Format italic
+            Font f1=new Font(Font.FontFamily.TIMES_ROMAN,10.0f,Font.ITALIC, BaseColor.BLACK);
+            Paragraph b1=new Paragraph(selectedText,f1);
+            document.add(new Paragraph(startingText));
+            document.add(b1);
+            document.add(new Paragraph(endingText));
+        }
+        else if(flag.equals("underline"))
+        {
+            //Format underline
+            Font f1=new Font(Font.FontFamily.TIMES_ROMAN,10.0f,Font.UNDERLINE, BaseColor.BLACK);
+            Paragraph b1=new Paragraph(selectedText,f1);
+            document.add(new Paragraph(startingText));
+            document.add(b1);
+            document.add(new Paragraph(endingText));
+        }
+        else
+        {
+            document.add(new Paragraph(mInputText.getText().toString()));
+
+        }
+
+        //addImagetoPDF();
 
         document.close();
         promptForNextAction();
     }
 
-    private void promptForNextAction() 
+
+    /**After pdf is created**/
+    private void promptForNextAction()
     {
         final String[] options = { "Email", "Preview", "Cancel" };
 
@@ -166,11 +369,9 @@ public class MainActivity extends AppCompatActivity {
             });
 
             builder.show();
-
-        
     }
 
-    private void emailNote() 
+    private void emailNote()
     {
         Intent email = new Intent(Intent.ACTION_SEND);
         email.putExtra(Intent.EXTRA_SUBJECT,"subject");
@@ -195,4 +396,84 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
+
+/*Image enhancement
+
+ private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            uploadImagetoFirebase();
+//            try {
+//                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+//
+//                //imageView.setImageBitmap(bitmap);
+//            }
+//            catch (IOException e)
+//            {
+//                e.printStackTrace();
+//            }
+        }
+    }
+
+    private void uploadImagetoFirebase() {
+
+        if(filePath != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            StorageReference ref = mStorageRef.child("images/"+ UUID.randomUUID().toString());
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(MainActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(MainActivity.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+        }
+    }
+
+    public void addImagetoPDF() throws IOException, DocumentException {
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        Bitmap bitmap = BitmapFactory.decodeResource(getBaseContext().getResources(), R.drawable.pdfimage);
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100 , stream);
+        Image myImg = Image.getInstance(stream.toByteArray());
+        myImg.setAlignment(Image.MIDDLE);
+
+        //add image to document
+        document.add(myImg);
+        mAttachImg.setImageResource(R.drawable.ic_plus_one_black_24dp);
+    }*/
+
+
 }
